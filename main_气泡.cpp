f9e76f868b3f6e1499568d8a31bb0118779643db
Benito Pentola <80289598+benitopentola@users.noreply.github.com>
2023-05-18 11:33:32 +0200

    Update main_气泡.cpp

--- main_气泡.cpp
+++ main_气泡.cpp
@@ -1,40 +1,35 @@
 #include <iostream>
-#include <cstdlib> // For rand() function
-#include <ctime> // For time() function
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main() {
     int size;
-    cout << "请输入一个1到1000之间的数字："; // Introduce un número entre 1 y 1000:
+    cout << "请输入一个1到1000之间的数字：";
     cin >> size;
 
-    // Validate that the size is within range (Complicado de leer si son demasiados numeros)
     if (size < 1 || size > 1000) {
-        cout << "数字必须在1到1000的范围内。" << endl; // El número debe estar en el rango de 1 a 1000.
+        cout << "数字必须在1到1000的范围内。" << endl;
         return 0;
     }
 
-    int array[size]; // Create the array with the entered size (¿Igual sería mejor presentarlo en lineas de 10)
+    int array[size];
 
-    // Generate random numbers and store them in the array (¿Varios arrays?)
-    srand(time(NULL)); // Initialize the random number generator seed (Investigar si hay metodos de generar aleatorio)
+    srand(time(NULL));
 
     for (int i = 0; i < size; i++) {
-        array[i] = rand() % 1000 + 1; // Generate a random number between 1 and 1000 
+        array[i] = rand() % 1000 + 1;
     }
 
-    // Print the generated random numbers (Buscar como imprimir de forma más legible)
     cout << "生成的随机数字：" << endl;
     for (int i = 0; i < size; i++) {
         cout << array[i] << endl;
     }
 
-    // Bubble Sort algorithm 
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
             if (array[j] > array[j + 1]) {
-                // Swap the elements 
                 int temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
@@ -42,10 +37,23 @@ int main() {
         }
     }
 
-    // Print the sorted numbers (Imprimir los números ordenados)
     cout << "排序后的数字：" << endl;
     for (int i = 0; i < size; i++) {
         cout << array[i] << endl;
+    }
+
+    cout << "按奇偶分类的结果：" << endl;
+    cout << "奇数：" << endl;
+    for (int i = 0; i < size; i++) {
+        if (array[i] % 2 != 0) {
+            cout << array[i] << endl;
+        }
+    }
+    cout << "偶数：" << endl;
+    for (int i = 0; i < size; i++) {
+        if (array[i] % 2 == 0) {
+            cout << array[i] << endl;
+        }
     }
 
     return 0;
