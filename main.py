42b531165496c026143c84e892d50063aba3c218
Benito Pentola <80289598+benitopentola@users.noreply.github.com>
2023-05-18 16:23:39 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# Asigna los valores de entrada
+h = 103
+w = 43
+d = 231
+
+# Calcula el área superficial y el volumen
+area = 2 * (h * w + h * d + w * d)
+volume = h * w * d
+
+# Muestra los resultados
+print(area, volume)
+
+
+
+
+
